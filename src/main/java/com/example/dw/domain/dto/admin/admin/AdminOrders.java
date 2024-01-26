ab28a78932303acbec316a5cf622c953431f2689
bokkaa <bokyh0327@gmail.com>
2024-01-26 14:09:34 +0900

    adminOrders

---
+++ src/main/java/com/example/dw/domain/dto/admin/admin/AdminOrders.java
@@ -0,0 +1,17 @@
+//package com.example.dw.domain.dto.admin.admin;
+//
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
+//public class AdminOrders {
+//
+//
+//
+//
+//    public static class
+//
+//
+//
+//}
